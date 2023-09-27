0613294a813ed037b26597890548eb874106408e
Hayden Hargreaves <hhargr836@west-mec.org>
2023-09-27 12:22:31 -0700

    User route should work, was not tested

--- api/routes.go
+++ api/routes.go
@@ -9,8 +9,20 @@ import (
 
 // Load routes in to the server
 func (server *Server) LoadRoutes (database Database) {
-	server.router.POST("/createApplication", func (ctx *gin.Context) { createApplication(ctx, database)})
-	server.router.POST("/createUser", createUser)
+	server.router.POST("/createApplication", func (ctx *gin.Context) { createApplication(ctx, database) })
+	server.router.POST("/createUser", func (ctx *gin.Context) { createUser(ctx, database) })
+}
+
+// Request map: createApplication
+type CreateApplicationRequest struct {
+	Name	string		`json:"name"`
+}
+
+// Request map: createUser
+type CreateUserRequest struct {
+	ApplicationID	uuid.UUID	`json:"applicationID"`
+	Username		string		`json:"username"`
+	Password		string		`json:"password"`
 }
 
 /*
@@ -25,24 +37,18 @@ func (server *Server) LoadRoutes (database Database) {
 		Name: string,
 		Key: uuid
 	}
-	
 */
-type CreateApplicationRequest struct {
-	Name string `json:"name"`
-}
-
 func createApplication (ctx *gin.Context, database Database) {
 	var appReq CreateApplicationRequest
 
 	if err := ctx.ShouldBindJSON(&appReq); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{ "error": err.Error() })
+		ctx.JSON(http.StatusBadRequest, gin.H{ "status": 400, "error": err.Error() })
 		return
 	}
 
 	application := *database.CreateApplication(appReq.Name)
 
 	ctx.JSON(http.StatusCreated, gin.H{ "status": 201, "application": &application })
-
 }
 
 /*
@@ -60,8 +66,16 @@ func createApplication (ctx *gin.Context, database Database) {
 		Username: string,
 		Password: string,
 	}
-
 */
-func createUser (ctx *gin.Context) {
+func createUser (ctx *gin.Context, database Database) {
+	var userReq CreateUserRequest
 
+	if err := ctx.ShouldBindJSON(&userReq); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{ "status": 400, "error": err.Error() })
+		return
+	}
+
+	user := *database.CreateUser(userReq.applicationID, userReq.username, userReq.password)
+
+	ctx.JSON(http.StatusCreated, gin.H{ "status": 201, "user": &user })
 }
