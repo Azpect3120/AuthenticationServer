d2e1646c163518d0591b65a33e3be9b3ad99792b
Azpect3120 <hhargreaves2006@gmail.com>
2023-10-02 21:06:33 -0700

    Added CORS middleware

--- internal/model/server.go
+++ internal/model/server.go
@@ -15,9 +15,26 @@ func CreateServer(port string, database Database) *Server {
 		port: port,
 	}
 	server.LoadRoutes(database)
+	server.loadCORS()
 	return server
 }
 
 func (server *Server) Listen () {
 	server.router.Run(":" + server.port)
+}
+
+func (server *Server) loadCORS () {
+	server.router.Use(func (c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
 }
